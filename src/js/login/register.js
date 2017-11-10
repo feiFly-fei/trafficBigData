bf3a0d240ca39f31a280969e9d21ba5ce77a9cf8
lifei <39952175@qq.com>
2017-11-10 15:30:26 +0800

    1

---
+++ src/js/login/register.js
@@ -0,0 +1,15 @@
+/**
+ * Created by lifei on 2017/11/10.
+ */
+
+$(function () {
+    $('#distpicker').distpicker({
+        province: "---- 请选择 ----",
+        city: "---- 不限 ----",
+        district: "---- 不限 ----"
+    });
+
+    $('.selectpicker').selectpicker({
+
+    });
+});
