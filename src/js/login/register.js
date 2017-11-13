f8b617e02fb8a726432ae12c0cc348c6ba6c10d9
lifei <39952175@qq.com>
2017-11-13 16:23:12 +0800

    注册页

--- src/js/login/register.js
+++ src/js/login/register.js
@@ -4,12 +4,23 @@
 
 $(function () {
     $('#distpicker').distpicker({
-        province: "---- 请选择 ----",
-        city: "---- 不限 ----",
-        district: "---- 不限 ----"
+        province: "请选择",
+        city: "不限",
+        district: "不限",
+        autoSelect: false
     });
 
     $('.selectpicker').selectpicker({
 
+    });
+
+    $('.toSecondBtn').click(function () {
+        $('.step-ul li.first').removeClass('selected').next('.second').addClass('selected');
+        $('.step-first-wrap').hide().next('.step-second-wrap').show();
+    });
+
+    $('.toThirdBtn').click(function () {
+        $('.step-ul li.second').removeClass('selected').next('.third').addClass('selected');
+        $('.step-second-wrap').hide().next('.step-third-wrap').show();
     });
 });
