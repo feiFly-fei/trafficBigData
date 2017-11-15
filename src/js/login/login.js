7ba7a627e639b19f4573c18140c1753e019e9810
lifei <39952175@qq.com>
2017-11-15 15:32:16 +0800

    问题修改

--- src/js/login/login.js
+++ src/js/login/login.js
@@ -6,6 +6,15 @@
 $(function () {
    $('.forgetPwd').click(function () {
        $('.forgetPwdModal').modal('toggle');
+
+       $('.forgetPwdModal').on('shown.bs.modal', function () {
+           var bodyHeight = $('body').height(),
+               modalHeight = $('.forgetPwdModal .modal-content').height();
+           var diff = (bodyHeight - modalHeight) / 2;
+           if(diff > 0){
+               $('.forgetPwdModal .modal-dialog').css('margin-top', diff);
+           }
+       });
    });
 
    $('#modifyPwdBtn').click(function () {
@@ -13,6 +22,15 @@ $(function () {
 
        setTimeout(function () {
            $('.remindModal').modal('toggle');
+
+           $('.remindModal').on('shown.bs.modal', function () {
+               var bodyHeight = $('body').height(),
+                   modalHeight = $('.remindModal .modal-content').height();
+               var diff = (bodyHeight - modalHeight) / 2;
+               if(diff > 0){
+                   $('.remindModal .modal-dialog').css('margin-top', diff);
+               }
+           });
        }, 500)
    });
 });
--- src/js/materialBigScreen.js
+++ src/js/materialBigScreen.js
@@ -491,7 +491,7 @@ $(function () {
                 nameMap: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
             },
             cellSize: 41,
-            left: 5,
+            left: 'center',
             range: '2017-01',
             itemStyle: {
                 normal: {
